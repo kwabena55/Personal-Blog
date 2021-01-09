8e1dcfda4f439e4abcc90f940ff4cda4bb6743d5
kwabena55 <solansah@gmail.com>
2021-01-09 10:38:13 -0700

    Added FIles

--- flaskblog.py
+++ flaskblog.py
@@ -6,12 +6,12 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template("templates/home.html")
+    return render_template("home.html")
 
 @app.route("/about")
 def about():
     return render_template("about.html")
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     app.run()
