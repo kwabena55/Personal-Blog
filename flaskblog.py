95fe52cc8837551ad586d0c8f6d940eea3d89abc
kwabena55 <solansah@gmail.com>
2021-01-09 03:38:14 -0700

    Added Files

--- flaskblog.py
+++ flaskblog.py
@@ -1,13 +1,17 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello():
-    return 'Hello, World!'
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template("templates/home.html")
+
+@app.route("/about")
+def about():
+    return "<h1> Home Page</h1>"
 
 
-
-if __name__=='__main__':
+if __name__=="__main__":
     app.run()
