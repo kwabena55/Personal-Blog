9959f892c5053bc7d8b61c4f1bb952e6044625d3
kwabena55 <solansah@gmail.com>
2021-01-09 11:36:50 -0700

    Updated Files

--- flaskblog.py
+++ flaskblog.py
@@ -1,12 +1,37 @@
 from flask import Flask,render_template
 
 app = Flask(__name__)
+posts=[
+
+    {
+        "author":"Solomon Ansah",
+
+        "title":"Blog Post",
+
+        "content": "My First Blog",
+
+        "date": "April 20, 2021"
+
+    },   
+    {
+        "author":"Jayden Ansah",
+
+        "title":"Blog Post2",
+
+        "content": "My Second Blog",
+
+        "date": "May 20, 2021"
+
+
+    }
+
+]
 
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template("home.html")
+    return render_template("home.html",posts=posts)
 
 @app.route("/about")
 def about():
